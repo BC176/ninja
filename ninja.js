8dd0f681355f8d4146521b237c8a17b9450816f4
Brian Clark <brian.w.clark176@gmail.com>
2021-08-26 16:34:37 -0600

    added parent/child classes, extend and inherit from parent class

--- ninja.js
+++ ninja.js
@@ -16,11 +16,37 @@ class Ninja {
         console.log(`drinkSake function invoked, 10 health has been added to ${this.name}.\n`)
         this.health += 10;
     }
-}
+}//end class Ninja
 
 const ninja = new Ninja("Chosen One", 50);
 const ninja2 = new Ninja("Great White Farley", 20);
 
+class Sensei extends Ninja {
+    constructor(name, health, speed, strength, wisdom) {
+        super(name, health, speed, strength, wisdom);
+        this.health = 200
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+
+    speakWisdom() {
+        const addToHealth = super.drinkSake();
+        // console.log(addToHealth);
+        console.log(`If my name wasn't ${this.name}, it would be something else.\n`)
+    }
+}//end class Sensei
+
+const sensei = new Sensei("Master Splinter");
+const sensei2= new Sensei("Shredder");
+
+//Testing outputs
+sensei.speakWisdom();
+sensei.showStats();
+sensei2.sayName();
+sensei2.showStats();
+sensei2.speakWisdom();
+ninja.showStats();
 ninja.sayName();
 ninja2.sayName();
 ninja.showStats();
@@ -29,3 +55,4 @@ ninja.drinkSake();
 ninja2.drinkSake();
 ninja.showStats();
 ninja2.showStats();
+sensei.sayName();
